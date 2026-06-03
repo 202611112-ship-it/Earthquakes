f252102702c5941b52e32d1f6f53939645c2ac95
202611112-ship-it <2026_11112@kshcm.net>
2026-06-03 11:23:06 +0700

    Update earthquake.py

--- earthquake.py
+++ earthquake.py
@@ -7,7 +7,7 @@ import joblib
 # -------------------------------
 # 데이터 불러오기
 # -------------------------------
-df_new = pd.read_csv("Earthquake.csv", encoding="utf-8")
+df_new = pd.read_csv("Earthquake.csv", encoding="cp949")
 
 # 컬럼명 통일
 df_new = df_new.rename(columns={
